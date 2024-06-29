705474134cb6f645821ae1db08a37f10f88e69bd
David Jepsson <david.jepsson@arcticgroup.se>
2024-06-29 16:43:50 +0200

    config timer

--- scripts/Countdown.gd
+++ scripts/Countdown.gd
@@ -1,17 +1,26 @@
 extends Control
 
-@onready var label = $Time
-@onready var timer = $CountdownTimer
+@export var countdown_time = 60 # Countdown time in seconds
+var time_left
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	timer.start()
+	print("Countdown Scene Ready")
+	time_left = countdown_time
+	$CountdownTimer.start()
+	update_timer_label()
 
-func time_left():
-	var time_left_var = timer.time_left
-	var second = int(time_left_var) % 60
-	return [second]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	label.text = "%02d" % time_left()
+	if time_left > 0:
+		time_left -= delta
+		update_timer_label()
+	else:
+		time_left = 0
+		update_timer_label()
+
+
+func update_timer_label():
+	var seconds = int(time_left) % 60
+	$Time.text = str(seconds)
