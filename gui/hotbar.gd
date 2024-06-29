8e3615c9f7cba628dd42de4a042aa57c6e9d3f49
Alexander Eklund <alexander460@gmail.com>
2024-06-29 16:40:09 +0200

    feat: add inventory gui and hotbar

---
+++ gui/hotbar.gd
@@ -0,0 +1,15 @@
+extends HBoxContainer
+
+
+#@onready var slots: Array = get_children()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func update() -> void:
+	#for i in range(slots.size()):
+	#	slots[i].update_to_slot()
+	pass
